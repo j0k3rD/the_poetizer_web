d38cf6049c92c3aab32785cfbc870553dc9bb712
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-03-22 19:18:34 -0300

    new ignores

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+#importar funcion que creara la app
+from main import create_app
 import os
-from dotenv import load_dotenv
 
-app=Flask(__name__)
-load_dotenv()
+#Llamar a la funcion y devolver la app
+app = create_app()
+#Hacer push sobre el contexto de la aplicacion
+#Esto permite acceder a las popiedades de la app
+app.app_context().push()
+
 
 if __name__=='__main__':
     app.run(debug=True, port = os.getenv('PORT'))
