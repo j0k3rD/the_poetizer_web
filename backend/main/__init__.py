b0607e397284dc32b3890d36492d427966374959
Aaron Moya <agm.moya@alumno.um.edu.ar>
2022-04-05 18:58:59 -0300

    add new recources, autor, poems, poem

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,13 +1,22 @@
 import os
+import resource
 from flask import Flask
 from dotenv import load_dotenv
+from flask_restful import Api
+import main.resources as resource
+
+api = Api()
 
 def create_app():
     #inicializar Flask
     app= Flask(__name__)
     #Cargar variables de entorno
     load_dotenv()
-    #
+    api.add_resource(resource.PoemsResource, '/poems')
+    api.add_resource(resource.PoemResource, '/poem/<id>')
+    api.add_resource(resource.AutorResource, '/autor/<id>')
     #Aqui se inicializaran el resto de los m
     #retornar aplicaciion inicializada
+    api.init_app(app)
     return app
+
