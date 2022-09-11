b0b3418ada3192416801ce353d902fc675787bc5
j0k3rD <agm.moya@alumno.um.edu.ar>
2022-09-11 19:40:14 -0300

    jinja users views ended

--- frontend/main/routes/user.py
+++ frontend/main/routes/user.py
@@ -3,11 +3,11 @@ from flask import Blueprint, render_template
 #Crear Blueprint
 user = Blueprint('user', __name__, url_prefix='/user')
 
-# USERS = [
-#             {"id":0,"firstname":"Pablo","lastname":"Sosa","email":"p.sosa@escuela.com"},
-#             {"id":1,"firstname":"María","lastname":"Moreno","email":"m.moreno@escuela.com", "private":True},
-#             {"id":2,"firstname":"Julieta","lastname":"Gonzales","email":"j.gonzales@escuela.com"}
-#             ]
+USERS = [
+            {"id":0,"firstname":"Pablo","lastname":"Sosa","email":"p.sosa@escuela.com"},
+            {"id":1,"firstname":"María","lastname":"Moreno","email":"m.moreno@escuela.com", "private":True},
+            {"id":2,"firstname":"Julieta","lastname":"Gonzales","email":"j.gonzales@escuela.com"}
+            ]
 
 @user.route('/details/<int:id>')
 def details(id):
