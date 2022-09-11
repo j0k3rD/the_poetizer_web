f5dc496108f5421c55f842bcd61aa57b01827ae3
j0k3rD <agm.moya@alumno.um.edu.ar>
2022-09-11 19:09:21 -0300

    jinja started

--- frontend/main/routes/poem.py
+++ frontend/main/routes/poem.py
@@ -4,20 +4,20 @@ from flask import Blueprint, render_template, url_for, redirect
 #Crear Blueprint
 poem = Blueprint('poem', __name__, url_prefix='/poem')
 
-POEMS = [
-            {"id":0,"poetname":"Pablo","title":"Red Roses","body":"Las rosas son rojas"},
-            {"id":1,"poetname":"Facundo","title":"Boca vos sos mi pasion","body":"Con un marchete en la mano, con la chapa en el corazon, seras siempre independiente, vigilante y boton", "private":True},
-            {"id":2,"poetname":"Emilia","title":"White Rose","body":"En el fin de los tiempos"},
-            ]
+# POEMS = [
+#             {"id":0,"poetname":"Pablo","title":"Red Roses","body":"Las rosas son rojas"},
+#             {"id":1,"poetname":"Facundo","title":"Boca vos sos mi pasion","body":"Con un marchete en la mano, con la chapa en el corazon, seras siempre independiente, vigilante y boton", "private":True},
+#             {"id":2,"poetname":"Emilia","title":"White Rose","body":"En el fin de los tiempos"},
+#             ]
 
 #Ver un poema determinado
-@poem.route('/view/<int:id>')
+@poem.route('/view_poet/<int:id>')
 def view_poet(id):
     #Mostrar template
     return render_template('view_poem_poet.html')
 
 #Ver un poema determinado
-@poem.route('/view/<int:id>')
+@poem.route('/view_user/<int:id>')
 def view_user(id):
     #Mostrar template
     return render_template('view_poem_user.html')
